3c216b76763a615f349b62b4d2167cafc87c5738
Nikita Bondarenko <11bender96@gmail.com>
2024-01-09 16:40:01 +0300

    feat: add react-navigation and ListScreen

---
+++ screen/ListScreen/ListScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const ListScreen: React.FC = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>ListScreen</Text>
+    </View>
+  );
+};
